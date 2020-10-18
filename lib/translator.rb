7fa8090cd830005e9e134b80058d1fd1a3eeeeff
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-18 21:55:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
     emoticon_mean['get_meaning'][japanese] = meaning
     emoticon_mean['get_emoticon'][english] = japanese
   end
-  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon = emoticon_mean['get_emoticon'][emoticon]
   japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
